b21b94dfe53ed7167731cefe0bcbc38415d7791c
01shiwangi <shiwangi_93@yahoo.com>
2017-02-04 22:06:33 -0500

    updation of tables

--- project6/src/java/generic/ApplicationConfig.java
+++ project6/src/java/generic/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
  *
  * @author John
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@javax.ws.rs.ApplicationPath("mobile")
 public class ApplicationConfig extends Application {
 
     @Override
